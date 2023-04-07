020ff0eb49695e2dc292096247bb5dbbcf6069ab
Hankerman <stalker-2204@mail.ru>
2023-04-07 20:00:56 +0300

    Добавьте файлы проекта.

---
+++ Socet2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Socet2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("207.46.197.32");
+            IPEndPoint ep = new IPEndPoint(ip, 80);
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            try
+            {
+                s.Connect(ep);
+                if (s.Connected)
+                {
+                    String strSend = "GET\r\n\r\n";
+                    s.Send(System.Text.Encoding.ASCII.GetBytes(strSend));
+                    byte[] buffer = new byte[1024];
+                    int l;
+
+                    do
+                    {
+                        l = s.Receive(buffer);
+                        Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer, 0, l));
+                    } while (l > 0);
+                }
+
+
+                Console.ReadLine();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
